f8810c94158f300f26d95be86dfa25322c198ad0
Sanket Mane <sanmane@sanmane-thinkpadp1gen7.pune.csb>
2025-08-06 11:40:12 +0530

    changes to the connection >> atlas

--- app/connections/mongo.connect.ts
+++ app/connections/mongo.connect.ts
@@ -1,15 +1,24 @@
 import mongoose from "mongoose";
 
-const mongoUri = "mongodb://localhost:27017/pink_slip_db";
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "";
+
+mongoose.set("strictQuery", false);
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoUri);
-    console.log("✅ Connected to local MongoDB");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
+    await mongoose.connect(MONGO_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions); // Needed for TypeScript
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("❌ MongoDB Atlas connection error:", error);
     process.exit(1);
   }
+
+  mongoose.connection.on("error", (err) => {
+    console.error("❌ MongoDB connection error event:", err);
+  });
 };
 
 export default connectToMongo;
